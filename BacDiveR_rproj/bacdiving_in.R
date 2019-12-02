36d539ed0890ef794fab3a8d907f9569de70aaa7
Tristan <tristan.caro@colorado.edu>
2019-12-01 23:09:33 -0700

    bacdive_2 with plots

--- BacDiveR_rproj/bacdiving_in.R
+++ BacDiveR_rproj/bacdiving_in.R
@@ -4,10 +4,10 @@ if(!require('devtools')) install.packages('devtools'); devtools::install_github(
 library(BacDiveR)
 library(magrittr)
 library(ggplot2)
-taxon_1 <- "Bacillus halodurans"
+taxon_1 <- "Dorea longicatena"
 Bac_data <- bd_retrieve_taxon(name = taxon_1)
 
-taxon_2 <- "Aneurinibacillus thermoaerophilus"
+taxon_2 <- "Fusicatenibacter saccharivorans"
 At_data <- bd_retrieve_taxon(name = taxon_2)
 
 extract_temps <- function(dataset, taxon_name) {
@@ -18,11 +18,19 @@ extract_temps <- function(dataset, taxon_name) {
     return()
 }
 
-temperature_Bac <- extract_temps(Bac_data, taxon_1) 
-temperature_At <- extract_temps(At_data, taxon_2)
+extract_gc <- function(dataset, taxon_name) {
+  purrr::map(.x = dataset, .f = ~.x$molecular_biology$GC_content) %>% 
+    unlist() %>%
+    as.numeric() %>%
+    data.frame(gc_cont = ., taxon = rep(taxon_name, length(.))) %>%
+    return()
+}
 
-rbind(temperature_Bac, temperature_At) %>% 
-  ggplot(aes(x = taxon, y = temp_C)) +
+gc_Bac <- extract_gc(Bac_data, taxon_1) 
+gc_At <- extract_gc(At_data, taxon_2)
+
+rbind(gc_Bac, gc_At) %>% 
+  ggplot(aes(x = taxon, y = gc_cont)) +
   geom_boxplot(notch = TRUE) +
   geom_jitter(height = 0, alpha = 0.5)
 #> notch went outside hinges. Try setting notch=FALSE.
